c52974de9de4e7c30b344126c492d00ef524dbcf
Daniel <dajoh16@student.sdu.dk>
2018-08-22 09:53:59 +0200

    A Test that totally doesn't fail

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -14,4 +14,13 @@ public class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    public void testThatFails(){
+        Item[] items = new Item[] {new Item("Sulfuras, Hand of Ragnaros", 2, 200)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(100,app.items[0].quality);
+    }
+
 }
